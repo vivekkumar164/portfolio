f2b87d497815f9c4a8de9466fb216a42298d4cbc
unknown <vivekkumar031096@gmail.com>
2022-10-19 21:11:09 +0530

    initial setup

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Intro.css'
+function Intro() {
+  return (
+    <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+                <span>Hy! I Am </span>
+                <span>Vivek kumar</span>
+                <span>Frontend Developer with high level of experience in web designing and development,
+                    producting the quality work.
+                </span>
+            </div>
+
+            <button className="i-button button">Hire me</button>
+        </div>
+        <div className="i-right"></div>
+    </div>
+  )
+}
+
+export default Intro
